ca2dd62fff7759b64d960d4d99f67e815d841eaa
TheFreeman15009 <hritik1509@gmail.com>
2020-10-08 18:22:37 +0530

    Fetch all theaters

--- routes/theater.js
+++ routes/theater.js
@@ -2,6 +2,20 @@ const express = require('express');
 const router = express.Router();
 const Theater = require('../models/Theatre')
 
+router.post('/theater/all',async (req,res)=>{
+    let output = {}
+    await Theater.find().sort({ createdAt: -1 })
+    .then((result)=>{
+         // console.log("then")
+         output = {result}
+    })
+    .catch((err)=>{
+         console.log(err)
+         output = {message:"Failed"};
+    })
+    res.send(output)
+});
+
 
 router.post('/theater/add',async (req,res)=>{
          let output = {}
