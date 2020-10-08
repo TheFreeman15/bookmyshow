06e10240d8a4db418aa98ddc295e21c57a9dd391
TheFreeman15009 <hritik1509@gmail.com>
2020-10-08 14:00:29 +0530

    fix

--- routes/movieroutes.js
+++ routes/movieroutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Movie = require('../models/Movie');
-const mongoose = require('mongoose');
 
 // Fetch all movies from DB
 router.get('/movies/all', async (req,res)=>{
