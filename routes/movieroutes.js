b398ced24559632aa2cd36e0680065e2bc909003
TheFreeman15009 <hritik1509@gmail.com>
2020-10-08 14:00:07 +0530

    Find movie by id

--- routes/movieroutes.js
+++ routes/movieroutes.js
@@ -1,23 +1,41 @@
 const express = require('express');
 const router = express.Router();
 const Movie = require('../models/Movie');
-
+const mongoose = require('mongoose');
 
 // Fetch all movies from DB
-router.get('/movies/all', (req,res)=>{
-     let result = {}
-     Movie.find().sort({ createdAt: -1 })
+router.get('/movies/all', async (req,res)=>{
+     let output = {}
+     await Movie.find().sort({ createdAt: -1 })
      .then((result)=>{
-          result = {movies:result}
+          // console.log("then")
+          output = {result}
      })
      .catch((err)=>{
           console.log(err)
-          result = {message:"Failed"};
+          output = {message:"Failed"};
      })
+     res.send(output)
 
 }) 
-// Add a movie to DB
+// Find a movie by id 
+router.get('/movies/find/:movieId', async (req,res)=>{
 
+     let output = {}
+     var id = req.params.movieId;
+     await Movie.findById(id)
+     .then((result)=>{
+          output = {result}
+     })
+     .catch((err)=>{
+          console.log(err)
+          output = {message:"Failed"}
+     })
+     res.send(output)
+}) 
+
+
+// Add a movie to DB
 router.post('/movie/add',async (req,res)=>{
          let output = {}
          const movie = new Movie(req.body)
